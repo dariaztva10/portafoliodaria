ee00229254f6b492b7dc51bc88732737ebd8fe69
dariaztva <dariaztva@gmail.com>
2025-03-14 17:02:32 +0100

    Correcciones y build generado

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,11 +4,11 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
+
 import { Skills } from "./pages/skills";
 import { Projects } from "./pages/projects";
 import { Contact } from "./pages/contact";
-import { Single } from "./pages/single";
+
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -31,11 +31,11 @@ const Layout = () => {
                     <Routes>
                     
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
+                        
                         <Route element={<Skills />} path="/skills" />
                         <Route element={<Projects />} path="/projects" />
                         <Route element={<Contact />} path="/contact" />
-                        <Route element={<Single />} path="/single/:theid" />
+                       
                         <Route element={<h1>Not found!</h1>} />
                       
                     </Routes>
