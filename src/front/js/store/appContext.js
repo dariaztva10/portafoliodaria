3a7890eba578a2fe592b43183a326e8a42fb6084
Daria Dacha Zotova <149780226+dariaztva10@users.noreply.github.com>
2025-10-31 22:33:59 +0100

    Implement conditional message fetching
    Add conditional fetching of messages based on BACKEND_URL.

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -29,6 +29,11 @@ const injectContext = PassedComponent => {
 			 * store, instead use actions, like this:
 			 **/
 			state.actions.getMessage(); // <---- calling this function from the flux.js actions
+			if (process.env.BACKEND_URL) {
+		    	state.actions.getMessage();
+		  	} else {
+		   		console.warn("BACKEND_URL no está definida. Saltando fetch.");
+		  	}
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
